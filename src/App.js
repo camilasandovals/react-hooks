64994300e0cfe45838d4a4ca3283609cfd82cbba
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-12 15:23:15 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import { FaStar } from "react-icons/fa"
 import './App.css';
 
+
+// functions to make work the star rating
 const createArray = (length) => [
   ...Array(length)
 ];
@@ -24,12 +26,15 @@ function StarRating( {totalStars = 1}){
   )
 }
 
+
+// main APP
 function App(props) {
-  const [status, setStatus] = useState("Not delivered");
+  const [status, setStatus] = useState("Not delivered")
   const [checked, setChecked] = useState(false)
   const [name, setName] = useState("Jan")
   const [loggedIn, setLoggedIn] = useState(false)
-
+  const [data, setData] = useState([])
+  //use effects 
   useEffect(() => {
     document.title = `Celebrate ${name}`
   }, [name]);
@@ -40,10 +45,20 @@ function App(props) {
     }`)
   }, [loggedIn])
 
-  return (
+  //use effect to load data
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+    .then((response) => response.json())
+    .then(setData);
+  }, []);
+
+  if(data){
+  return ( 
     <div>
+      {/* props */}
       <h1>Hello {props.name}.</h1>
       <p>The package is: {status}</p>
+      {/* useState */}
       <button onClick={() => setStatus("Delivered")}>Deliver</button>
       <div>
         <input type="checkbox"
@@ -51,9 +66,12 @@ function App(props) {
         onChange={() => setChecked(!checked)}/>
         <p>{checked? "checked" : "not checked"}</p>
       </div>
+      {/* stars */}
       <div>
         <StarRating totalStars={5}/>
       </div>
+
+      {/* useeffect */}
       <section>
         Congratulations {name}
         <button onClick={() => setName("Will")}>Change winner</button>
@@ -62,8 +80,19 @@ function App(props) {
           <button onClick={() => {setLoggedIn(true)}}>Log in</button>
         </div>
       </section>
+
+      {/* loaddata */}
+      <main>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+      </main>
+      <button onClick={() => setData([])}>Remove data</button>
     </div>
   );
+}
 }
 
 export default App;
