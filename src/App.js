4611e1c0cee1b5ef45ca86777a5f958add24a61c
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-12 13:34:18 -0400

    useState for checkbox

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 function App(props) {
   const [status, setStatus] = useState("Not delivered");
-  
+  const [checked, setChecked] = useState(false)
   return (
     <div>
       <h1>Hello {props.name}.</h1>
       <p>The package is: {status}</p>
       <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <div>
+        <input type="checkbox"
+        value={checked}
+        onChange={() => setChecked(!checked)}/>
+        <p>{checked? "checked" : "not checked"}</p>
+      </div>
     </div>
   );
 }
