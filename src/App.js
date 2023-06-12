e0ab76e9e935588af9af50e70692d8a76ab6bc74
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-12 13:27:56 -0400

    use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App(props) {
-  console.log()
+  const [status, setStatus] = useState("Not delivered");
+  
   return (
-    <div className="App">
+    <div>
       <h1>Hello {props.name}.</h1>
+      <p>The package is: {status}</p>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
 const people = ["Alex", "Ali", "Anna"]
 console.log(people[1])
+
+//destructuring
+const [first, ,third] = ["Alex", "Ali", "Anna"]
+console.log(third)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
